53deeb998767ed6672bbb7dd46bc94c1420602c7
Jitendra Patel <54883464+jitendrajp@users.noreply.github.com>
2025-12-20 20:23:46 +0530

    Seed users in DatabaseSeeder
    Added user seeding data for initial database setup.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $users = [
+            [
+                'name'  => 'User Alpha',
+                'email' => 'user.alpha@demo.app',
+            ],
+            [
+                'name'  => 'User Beta',
+                'email' => 'user.beta@demo.app',
+            ],
+            [
+                'name'  => 'User Gamma',
+                'email' => 'user.gamma@demo.app',
+            ],
+        ];
+
+        foreach ($users as $userData) {
+            User::updateOrCreate(
+                ['email' => $userData['email']], // unique key
+                [
+                    'name'              => $userData['name'],
+                    'password'          => Hash::make('mini@Wallet025'),
+                    'balance'           => 1000,
+                    'email_verified_at' => now(),
+                ]
+            );
+        }
 
     }
 }
